abc57fd2616855058917e882c148feadddb755e2
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-29 21:22:40 -0500

    Changed shopping cart icon to one that has better contrast with background-color of nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import logoOutline from './assets/logo-with-outline.png';
-import shoppingIcon from './assets/shopping-outline.svg';
+import cartIcon from './assets/green-cart.png';
 import Home from './components/Home';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
             <li>Home</li>
             <li>Products</li>
             <li>About Us</li>
-            <li><img className='shopping-icon' src={shoppingIcon} alt='icon of shopping bag' /></li>
+            <li><img className='shopping-icon' src={cartIcon} alt='icon of shopping bag' /></li>
           </ul>
         </nav>
       <Home />
