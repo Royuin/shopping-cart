2dcf7de092d1303c0c845cff5cd09f5b389e812d
Gage <77414756+Royuin@users.noreply.github.com>
2023-07-01 16:33:56 -0500

    Set up nested routes for products and Products as index and ProductsLayout as the parent

--- src/App.js
+++ src/App.js
@@ -4,29 +4,36 @@ import logoOutline from './assets/logo-with-outline.png';
 import cartIcon from './assets/green-cart.png';
 import Home from './components/Home';
 import Products from './components/Products';
+import Matcha from './components/Matcha';
+import ProductsLayout from './ProductsLayout';
 
 function App() {
   return (
     <div className="App">
-        <nav className='site-nav' aria-label='site'>
+      <nav className='site-nav' aria-label='site'>
         <div className='left-nav'>
-        <img className='header-logo' src={logoOutline} alt='Logo of a tea kettle with matcha tea on the side.' />
-        <h1> Reverie Tea </h1>
+          <img className='header-logo' src={logoOutline} alt='Logo of a tea kettle with matcha tea on the side.' />
+          <h1> Reverie Tea </h1>
         </div>
-          <ul>
-            <li><Link to='/'>Home</Link></li>
-            <li><Link to='/products'>Products</Link></li>
-            <li>About Us</li>
-            <li><img className='shopping-icon' src={cartIcon} alt='icon of shopping bag' /></li>
-          </ul>
-        </nav>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/products'>Products</Link></li>
+          <li>About Us</li>
+          <li><img className='shopping-icon' src={cartIcon} alt='icon of shopping bag' /></li>
+        </ul>
+      </nav>
+
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/products' element={<Products />} />
+        <Route path='/products' element={<ProductsLayout />}>       
+          <Route index element={<Products />} />
+          <Route path='matcha' element={<Matcha />} />
+        </Route>
       </Routes>
+
       <footer>
         <img src={logoOutline} alt='Logo of a tea kettle with matcha tea on the side.' />
-      <h5>Reverie Tea</h5> 
+        <h5>Reverie Tea</h5> 
         <p>Copyright &copy; <a href='https://github.com/Royuin'> Royuin </a></p>
         <a href="https://www.flaticon.com/free-icons/smart-cart" title="smart cart icons">Smart cart icons created by Muhammad_Usman - Flaticon</a>
         <br />
