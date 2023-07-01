91a7959c0702d33575d8586e59427a834530fa9a
Gage <77414756+Royuin@users.noreply.github.com>
2023-07-01 16:28:59 -0500

    Removed header and aside from Products component and placed inside to new file ProductsLayout so all different product routes can use it as a base and only change the products

---
+++ src/ProductsLayout.js
@@ -0,0 +1,28 @@
+import {Routes, Route, Outlet, Link} from 'react-router-dom';
+import matchaTableWhisk from './assets/matcha-table-whisk.jpg';
+
+function ProductsLayout() {
+ return (
+ <>
+    <header className='products-header'>
+      <img src={matchaTableWhisk} alt='Matcha powder and whisk on a table' />
+      <h2 className='our-products-heading'>Our Products</h2>
+    </header>
+    <aside>
+      <nav aria-label='products'>
+        <ul>
+          <li><Link to='/products'>All Products</Link></li>
+          <li><Link to='/products/matcha'>Matcha</Link></li>
+          <li>Green Teas</li>
+          <li>Black Teas</li>
+          <li>Herbal</li>
+          <li>Accessories</li>
+        </ul>
+      </nav>
+    </aside>
+      <Outlet />
+  </>
+  )
+}
+
+export default ProductsLayout;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,28 +1,12 @@
-import matchaTableWhisk from '../assets/matcha-table-whisk.jpg';
-import {Link} from 'react-router-dom';
 import '../styles/Products.css';
 
 function Products() {
   return (
-    <>
-      <header className='products-header'>
-        <img src={matchaTableWhisk} alt='Matcha powder and whisk on a table' />
-        <h2 className='our-products-heading'>Our Products</h2>
-      </header>
-      <aside>
-        <nav aria-label='products'>
-          <ul>
-            <li><Link path='/products'>All Products</Link></li>
-          <li>Matcha</li>
-          <li>Green Teas</li>
-          <li>Black Teas</li>
-          <li>Herbal</li>
-            <li>Accessories</li>
-          </ul>
-        </nav>
-      </aside>
-    </>
+  <main>
+  <h2>All Products</h2>
+  </main>
   )
+
 }
 
 export default Products;
