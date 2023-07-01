262a2c85ba5bb2a8670020ca237b43dada3bd782
Gage <77414756+Royuin@users.noreply.github.com>
2023-06-30 20:50:51 -0500

    Created section for explaining a quick summary of company and a button that in the future route user to about us page

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,6 +30,15 @@ function Home() {
             </div>
           </div>
         </section>
+        <section className='why-us-section'>
+          <h2 className='why-us-heading'> Why Us? </h2>
+          <div className='why-us-wrapper'>
+            <h4>You see this is where text would go to explain why but, this company doesn't actually exist. So here is placeholder text!</h4>
+            <br />
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <button className='learn-button'>Learn More</button>
+          </div>
+        </section>
       </main>
     </>
   )
