ab7710e0f6f72e4bc71352f3051a4a6de0552d61
Gage <77414756+Royuin@users.noreply.github.com>
2023-07-01 11:50:46 -0500

    Formatting changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,7 +35,8 @@ function Home() {
           <div className='why-us-wrapper'>
             <h4>You see this is where text would go to explain why but, this company doesn't actually exist. So here is placeholder text!</h4>
             <br />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <button className='learn-button'>Learn More</button>
           </div>
         </section>
