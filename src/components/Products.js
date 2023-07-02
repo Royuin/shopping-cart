26db513e184ac934a9c7d4a5fab506457d03fadf
Gage <77414756+Royuin@users.noreply.github.com>
2023-07-02 10:05:14 -0500

    Created products state to store all products and made function to render all products in a grid with 2 columns at most

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,47 @@
+import { useState } from 'react';
 import '../styles/Products.css';
+import uniqid from 'uniqid';
+import jasmine from '../assets/jasmine-leaves.jpeg';
+import chamomile from '../assets/chamomile-leaves.jpg';
 
 function Products() {
+
+  const [products, setProducts] = useState([
+    { name: 'Green Jasmine',
+      src: jasmine,
+      type: 'green',
+      description: 'This is where it would go if you had one and a real product',
+      price: 15.65,
+      id: uniqid(),
+    },
+    { name: 'Chamomile',
+      src: chamomile,
+      type: 'herbal',
+      description: 'This is where it would go if you had one and a real product',
+      price: 12.39,
+      id: uniqid(),
+    }
+  ]);
+
+  function renderProducts() {
+   const productItems = products.map((product) => {
+   return (
+    <li key={product.id}>
+    <img src={product.src} alt='' />
+    <p>{product.name}</p>
+    <p>${product.price}</p>
+    </li>
+    )});
+
+    return <ul className='product-list'> {productItems}</ul>
+  }
+
   return (
-  <main>
-  <h2>All Products</h2>
-  </main>
+    <main>
+      <h2>All Products</h2>
+      <p><em>Price is per 3 oz </em></p>
+      {renderProducts()}
+    </main>
   )
 
 }
