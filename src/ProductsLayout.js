d32f65932775466c82a70a1ef51627f5c3b41ff3
Gage <77414756+Royuin@users.noreply.github.com>
2023-07-02 12:54:11 -0500

    Moved products array from Product.js to ProductLayout so it can pass array to other categories routes

--- src/ProductsLayout.js
+++ src/ProductsLayout.js
@@ -1,29 +1,82 @@
 import {Routes, Route, Outlet, Link} from 'react-router-dom';
 import matchaTableWhisk from './assets/matcha-table-whisk.jpg';
+import { useState } from 'react';
+import jasmine from './assets/jasmine-leaves.jpeg';
+import chamomile from './assets/chamomile-leaves.jpg';
+import black from './assets/black-tea-leaves.jpg';
+import ceylon from './assets/ceylon-tea.jpg';
+import rooibos from './assets/rooibos-tea.jpg';
+import mallow from './assets/mallow-flowers.jpg';
+import uniqid from 'uniqid';
 
 function ProductsLayout() {
- return (
- <>
-    <header className='products-header'>
-      <img src={matchaTableWhisk} alt='Matcha powder and whisk on a table' />
-      <h2 className='our-products-heading'>Our Products</h2>
-    </header>
+  const [products, setProducts] = useState([
+    { name: 'Green Jasmine',
+      src: jasmine,
+      type: 'green',
+      description: 'This is where it would go if you had one and a real product',
+      price: 15.65,
+      id: uniqid(),
+    },
+    { name: 'Chamomile',
+      src: chamomile,
+      type: 'herbal',
+      description: 'This is where it would go if you had one and a real product',
+      price: 12.39,
+      id: uniqid(),
+    },
+    { name: 'Black',
+      src: black,
+      type: 'black',
+      description: 'This is where it would go if you had one and a real product',
+      price: 13.72,
+      id: uniqid(),
+    },
+    { name: 'Ceylon Green',
+      src: ceylon,
+      type: 'green',
+      description: 'This is where it would go if you had one and a real product',
+      price: 17.24,
+      id: uniqid(),
+    },
+    { name: 'Rooibos',
+      src: rooibos,
+      type: 'herbal',
+      description: 'This is where it would go if you had one and a real product',
+      price: 17.73,
+      id: uniqid(),
+    },
+    {name: 'Mallow',
+      src: mallow,
+      type: 'herbal',
+      description: 'This is where it would go if you had one and a real product',
+      price: 10.79,
+      id: uniqid(),
+    }
+  ])
+
+  return (
+    <>
+      <header className='products-header'>
+        <img src={matchaTableWhisk} alt='Matcha powder and whisk on a table' />
+        <h2 className='our-products-heading'>Our Products</h2>
+      </header>
       <div className='products-with-aside'>
-    <aside>
-      <nav aria-label='products'>
-        <ul>
-          <li><Link to='/products'>All Products</Link></li>
-          <li><Link to='/products/matcha'>Matcha</Link></li>
-          <li>Green Teas</li>
-          <li>Black Teas</li>
-          <li>Herbal</li>
-          <li>Accessories</li>
-        </ul>
-      </nav>
-    </aside>
-      <Outlet />
+        <aside>
+          <nav aria-label='products'>
+            <ul>
+              <li><Link to='/products'>All Products</Link></li>
+              <li><Link to='/products/matcha'>Matcha</Link></li>
+              <li>Green Teas</li>
+              <li>Black Teas</li>
+              <li>Herbal</li>
+              <li>Accessories</li>
+            </ul>
+          </nav>
+        </aside>
+        <Outlet context={products} />
       </div>
-  </>
+    </>
   )
 }
 
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,37 +1,18 @@
-import { useState } from 'react';
 import '../styles/Products.css';
-import uniqid from 'uniqid';
-import jasmine from '../assets/jasmine-leaves.jpeg';
-import chamomile from '../assets/chamomile-leaves.jpg';
+import { useOutletContext } from 'react-router-dom';
 
 function Products() {
-
-  const [products, setProducts] = useState([
-    { name: 'Green Jasmine',
-      src: jasmine,
-      type: 'green',
-      description: 'This is where it would go if you had one and a real product',
-      price: 15.65,
-      id: uniqid(),
-    },
-    { name: 'Chamomile',
-      src: chamomile,
-      type: 'herbal',
-      description: 'This is where it would go if you had one and a real product',
-      price: 12.39,
-      id: uniqid(),
-    }
-  ]);
+  const products = useOutletContext();
 
   function renderProducts() {
-   const productItems = products.map((product) => {
-   return (
-    <li key={product.id}>
-    <img src={product.src} alt='' />
-    <p>{product.name}</p>
-    <p>${product.price}</p>
-    </li>
-    )});
+    const productItems = products.map((product) => {
+      return (
+        <li key={product.id}>
+          <img src={product.src} alt='' />
+          <p>{product.name}</p>
+          <p>${product.price}</p>
+        </li>
+      )});
 
     return <ul className='product-list'> {productItems}</ul>
   }
