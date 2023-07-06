9336b23e278f43e1fc5f872b77a50e188692c7a5
Gage <77414756+Royuin@users.noreply.github.com>
2023-07-05 20:47:46 -0500

    Added more products and change product array name from products to productsArray

--- src/ProductsLayout.js
+++ src/ProductsLayout.js
@@ -7,12 +7,17 @@ import black from './assets/black-tea-leaves.jpg';
 import ceylon from './assets/ceylon-tea.jpg';
 import rooibos from './assets/rooibos-tea.jpg';
 import mallow from './assets/mallow-flowers.jpg';
+import xianzhi from './assets/xianzhi-green.jpg';
+import reverieGreenBlend from './assets/reverie-green-blend.jpg';
+import matchaCooking from './assets/matcha-cooking-powder.jpg';
+import matchaCeremonial from './assets/matcha-ceremonial-powder.jpg';
 import uniqid from 'uniqid';
 
 function ProductsLayout() {
-  const [products, setProducts] = useState([
+  const [productsArray, setProductsArray] = useState([
     { name: 'Green Jasmine',
       src: jasmine,
+      alt: 'dried green jasmine tea leaves',
       type: 'green',
       description: 'This is where it would go if you had one and a real product',
       price: 15.65,
@@ -20,6 +25,7 @@ function ProductsLayout() {
     },
     { name: 'Chamomile',
       src: chamomile,
+      alt: 'dried chamomile flowsers',
       type: 'herbal',
       description: 'This is where it would go if you had one and a real product',
       price: 12.39,
@@ -27,6 +33,7 @@ function ProductsLayout() {
     },
     { name: 'Black',
       src: black,
+      alt: 'dried black tea leaves',
       type: 'black',
       description: 'This is where it would go if you had one and a real product',
       price: 13.72,
@@ -34,6 +41,7 @@ function ProductsLayout() {
     },
     { name: 'Ceylon Green',
       src: ceylon,
+      alt: 'dried dark green tea leaves',
       type: 'green',
       description: 'This is where it would go if you had one and a real product',
       price: 17.24,
@@ -41,6 +49,7 @@ function ProductsLayout() {
     },
     { name: 'Rooibos',
       src: rooibos,
+      alt: 'Dried red and orange tea leaves with bits of green',
       type: 'herbal',
       description: 'This is where it would go if you had one and a real product',
       price: 17.73,
@@ -48,9 +57,42 @@ function ProductsLayout() {
     },
     {name: 'Mallow',
       src: mallow,
+      alt: 'Dried purple flowers with green stems',
       type: 'herbal',
       description: 'This is where it would go if you had one and a real product',
       price: 10.79,
+      id: uniqid(),
+    },
+    {name: 'Xianzhi Green',
+      src: xianzhi,
+      alt: 'Long dried jade green leaves',
+      type: 'green',
+      description: 'This is where it would go if you had one and a real product',
+      price: 21.97,
+      id: uniqid(),
+    },
+    { name: 'Reverie Green Blend',
+      src: reverieGreenBlend,
+      alt: 'Dried green leaves with bits of blue',
+      type: 'green',
+      description: 'This is where it would go if you had one and a real product',
+      price: 18.14,
+      id: uniqid(),
+    },
+    { name: 'Matcha Cooking Grade',
+      src: matchaCooking,
+      alt: 'Dull green powder',
+      type: 'matcha',
+      description: 'This is where it would go if you had one and a real product',
+      price: 23.28,
+      id: uniqid(),
+    },
+    { name: 'Matcha Ceremonial Grade',
+      src: matchaCeremonial,
+      alt: 'Bright green powder',
+      type: 'matcha',
+      description: 'This is where it would go if you had one and a real product',
+      price: 29.59,
       id: uniqid(),
     }
   ])
@@ -67,14 +109,14 @@ function ProductsLayout() {
             <ul>
               <li><Link to='/products'>All Products</Link></li>
               <li><Link to='/products/matcha'>Matcha</Link></li>
-              <li>Green Teas</li>
+              <li><Link to='/products/green-tea'>Green Teas</Link></li>
               <li>Black Teas</li>
               <li>Herbal</li>
               <li>Accessories</li>
             </ul>
           </nav>
         </aside>
-        <Outlet context={products} />
+        <Outlet context={productsArray} />
       </div>
     </>
   )
