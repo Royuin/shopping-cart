57251c785ef16eec1e7339f603e302a5203a8186
Gage <77414756+Royuin@users.noreply.github.com>
2023-07-08 23:39:47 -0500

    Removed productsArray from ProductsLayout

--- src/ProductsLayout.js
+++ src/ProductsLayout.js
@@ -14,89 +14,6 @@ import matchaCeremonial from './assets/matcha-ceremonial-powder.jpg';
 import uniqid from 'uniqid';
 
 function ProductsLayout() {
-  const [productsArrayContext, setProductsArrayContext] = useState([
-    { name: 'Green Jasmine',
-      src: jasmine,
-      alt: 'dried green jasmine tea leaves',
-      type: 'green',
-      description: 'This is where it would go if you had one and a real product',
-      price: 15.65,
-      id: uniqid(),
-    },
-    { name: 'Chamomile',
-      src: chamomile,
-      alt: 'dried chamomile flowsers',
-      type: 'herbal',
-      description: 'This is where it would go if you had one and a real product',
-      price: 12.39,
-      id: uniqid(),
-    },
-    { name: 'Black',
-      src: black,
-      alt: 'dried black tea leaves',
-      type: 'black',
-      description: 'This is where it would go if you had one and a real product',
-      price: 13.72,
-      id: uniqid(),
-    },
-    { name: 'Ceylon Green',
-      src: ceylon,
-      alt: 'dried dark green tea leaves',
-      type: 'green',
-      description: 'This is where it would go if you had one and a real product',
-      price: 17.24,
-      id: uniqid(),
-    },
-    { name: 'Rooibos',
-      src: rooibos,
-      alt: 'Dried red and orange tea leaves with bits of green',
-      type: 'herbal',
-      description: 'This is where it would go if you had one and a real product',
-      price: 17.73,
-      id: uniqid(),
-    },
-    {name: 'Mallow',
-      src: mallow,
-      alt: 'Dried purple flowers with green stems',
-      type: 'herbal',
-      description: 'This is where it would go if you had one and a real product',
-      price: 10.79,
-      id: uniqid(),
-    },
-    { name: 'Xianzhi Green',
-      src: xianzhi,
-      alt: 'Long dried jade green leaves',
-      type: 'green',
-      description: 'This is where it would go if you had one and a real product',
-      price: 21.97,
-      id: uniqid(),
-    },
-    { name: 'Reverie Green Blend',
-      src: reverieGreenBlend,
-      alt: 'Dried green leaves with bits of blue',
-      type: 'green',
-      description: 'This is where it would go if you had one and a real product',
-      price: 18.14,
-      id: uniqid(),
-    },
-    { name: 'Matcha Cooking Grade',
-      src: matchaCooking,
-      alt: 'Dull green powder',
-      type: 'matcha',
-      description: 'This is where it would go if you had one and a real product',
-      price: 23.28,
-      id: uniqid(),
-    },
-    { name: 'Matcha Ceremonial Grade',
-      src: matchaCeremonial,
-      alt: 'Bright green powder',
-      type: 'matcha',
-      description: 'This is where it would go if you had one and a real product',
-      price: 29.59,
-      id: uniqid(),
-    }
-  ])
-
   return (
     <>
       <header className='products-header'>
@@ -112,7 +29,6 @@ function ProductsLayout() {
               <li><Link to='/products/green-tea'>Green Teas</Link></li>
               <li>Black Teas</li>
               <li>Herbal</li>
-              <li>Accessories</li>
             </ul>
           </nav>
         </aside>
