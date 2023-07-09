c631d6122d2e4d78770aaeae0072140b3717ff60
Gage <77414756+Royuin@users.noreply.github.com>
2023-07-09 13:59:53 -0500

    Changed products array to be received as a prop instead of useOutletContext

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,27 +1,23 @@
 import '../styles/Products.css';
-import { useOutletContext } from 'react-router-dom';
 
-function Products() {
-
-  const productsArray = useOutletContext();
+function Products({productsArray}) {
 
   function renderProducts() {
     const productItems = productsArray.map((product) => {
       return (
-        <li key={product.id}>
+        <li key={product.id} >
           <img src={product.src} alt={product.alt} />
           <p>{product.name}</p>
           <p>${product.price}</p>
         </li>
       )});
-
     return <ul className='product-list'> {productItems}</ul>
   }
 
   return (
     <main>
       <h2>All Products</h2>
-      <p><em>Price is per 3 oz </em></p>
+      <p><em>Price is per 3 oz</em></p>
       {renderProducts()}
     </main>
   )
