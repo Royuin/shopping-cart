7da4b0a58bd58a478300bb264e2c89fe2b0ce859
Gage <77414756+Royuin@users.noreply.github.com>
2023-07-12 21:06:58 -0500

    Added more to product description and wrapped details and steeping instructions in their own div wrappers

--- src/components/ProductDetailsLayout.js
+++ src/components/ProductDetailsLayout.js
@@ -23,11 +23,15 @@ function ProductDetailsLayout() {
         <img src={product['src']} alt={product.alt}/>
         <div className='details-right'>
           <h2>{id}</h2>
-          <h4>Tasting Notes: Buzzword, Buzzword, Buzzword</h4>
-          <h4>Details</h4>
-          <p>{product.description}</p>
-          <h4>Steeping Instructions</h4> 
-          <p>Heat water to 180{'\u00b0'} use 8 oz water for one teaspoon. Let it steep for 3-5 minutes and enjoy!</p>
+          <h4>Tasting Notes: <span>Buzzword, Buzzword, Buzzword</span></h4>
+          <div classname='details-wrap'>
+            <h4>Details</h4>
+            <p>{product.description}. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div className='steeping-wrap'>
+            <h4>Steeping Instructions</h4> 
+            <p>Heat water to 180{'\u00b0'} use 8 oz water for one teaspoon. Let it steep for 3-5 minutes and enjoy!</p>
+          </div>
           <br />
           <p>Price per 3 oz: ${product.price}</p>
           <label for='quantity'>
