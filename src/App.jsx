df2b4487caa26e7ed3be51bd1cbcff9c45d0ba23
Gage <77414756+Royuin@users.noreply.github.com>
2023-10-03 20:06:09 -0500

    Created a seperate component for the navbar and removed navbar markup from App.jsx to help reduce bloat, and better seperate different parts of app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import uniqid from 'uniqid';
 import logoOutline from './assets/logo-with-outline.png';
-import cartIcon from './assets/green-cart.png';
 import Home from './components/Home';
 import About from './components/About';
 import Cart from './components/Cart';
@@ -24,6 +23,7 @@ import reverieGreenBlend from './assets/reverie-green-blend.jpg';
 import matchaCooking from './assets/matcha-cooking-powder.jpg';
 import matchaCeremonial from './assets/matcha-ceremonial-powder.jpg';
 import BlackTea from './components/Black';
+import Navbar from './components/Navbar';
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -223,80 +223,13 @@ function App() {
     return setCart(updatedCart);
   }
 
-  function renderCartLink() {
-    if (cart.length > 0) {
-      return (
-        <li className='cart-icon-wrapper'>
-          <Link to='/cart' className='cart-link'>
-            <p>{cart.length}</p>
-            <img
-              className='shopping-icon'
-              src={cartIcon}
-              alt='icon of a green shopping cart'
-            />
-          </Link>
-        </li>
-      );
-    }
-    return (
-      <li className='cart-link'>
-        <Link to='/cart'>
-          <img
-            className='shopping-icon'
-            src={cartIcon}
-            alt='icon of a green shopping cart'
-          />
-        </Link>
-      </li>
-    );
-  }
-
   return (
     <div className='App'>
-      <nav className='site-nav' aria-label='site'>
-        <div className='left-nav'>
-          <img
-            className='header-logo'
-            src={logoOutline}
-            alt='Logo of a tea kettle with matcha tea on the side.'
-          />
-          <h1> Reverie Tea </h1>
-        </div>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li className='products-dropdown-wrapper'>
-            <button
-              className='nav-products-button '
-              onClick={toggleProductsDropdown}
-            >
-              Products
-            </button>
-            <ul className={'products-dropdown ' + dropdown}>
-              <li>
-                <Link to='/products'>All Products</Link>
-              </li>
-              <li>
-                <Link to='/products/green-tea'>Green Tea</Link>
-              </li>
-              <li>
-                <Link to='/products/matcha'>Matcha</Link>
-              </li>
-              <li>
-                <Link to='/products/black-tea'>Black Tea</Link>
-              </li>
-              <li>
-                <Link to='/products/herbal-tea'>Herbal Tea</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to='/about'>About Us</Link>
-          </li>
-          {renderCartLink()}
-        </ul>
-      </nav>
+      <Navbar
+        cartLength={cart.length}
+        toggleProductsDropdown={toggleProductsDropdown}
+        dropdown={dropdown}
+      />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/products' element={<ProductsLayout />}>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,82 @@
+import { Link } from 'react-router-dom';
+import cartIcon from '../assets/green-cart.png';
+import logoOutline from '../assets/logo-with-outline.png';
+
+function Navbar({ cartLength, toggleProductsDropdown, dropdown }) {
+  function renderCartLink() {
+    if (cartLength > 0) {
+      return (
+        <li className='cart-icon-wrapper'>
+          <Link to='/cart' className='cart-link'>
+            <p>{cartLength}</p>
+            <img
+              className='shopping-icon'
+              src={cartIcon}
+              alt='icon of a green shopping cart'
+            />
+          </Link>
+        </li>
+      );
+    }
+    return (
+      <li className='cart-link'>
+        <Link to='/cart'>
+          <img
+            className='shopping-icon'
+            src={cartIcon}
+            alt='icon of a green shopping cart'
+          />
+        </Link>
+      </li>
+    );
+  }
+
+  return (
+    <nav className='site-nav' aria-label='site'>
+      <div className='left-nav'>
+        <img
+          className='header-logo'
+          src={logoOutline}
+          alt='Logo of a tea kettle with matcha tea on the side.'
+        />
+        <h1> Reverie Tea </h1>
+      </div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li className='products-dropdown-wrapper'>
+          <button
+            className='nav-products-button '
+            onClick={toggleProductsDropdown}
+          >
+            Products
+          </button>
+          <ul className={'products-dropdown ' + dropdown}>
+            <li>
+              <Link to='/products'>All Products</Link>
+            </li>
+            <li>
+              <Link to='/products/green-tea'>Green Tea</Link>
+            </li>
+            <li>
+              <Link to='/products/matcha'>Matcha</Link>
+            </li>
+            <li>
+              <Link to='/products/black-tea'>Black Tea</Link>
+            </li>
+            <li>
+              <Link to='/products/herbal-tea'>Herbal Tea</Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link to='/about'>About Us</Link>
+        </li>
+        {renderCartLink()}
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
