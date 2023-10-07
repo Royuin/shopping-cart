71c97920e6525f59968f95f8964872555cd60b27
Gage <77414756+Royuin@users.noreply.github.com>
2023-10-06 20:10:34 -0500

    Made products page responsive for mobile devices

--- src/ProductsLayout.jsx
+++ src/ProductsLayout.jsx
@@ -1,29 +1,37 @@
-import { Outlet, Link} from 'react-router-dom';
-import matchaTableWhisk from './assets/matcha-table-whisk.jpg';
+import { Outlet, Link } from 'react-router-dom';
 
 function ProductsLayout() {
   return (
     <>
       <header className='products-header'>
-        <img src={matchaTableWhisk} alt='Matcha powder and whisk on a table' />
         <h2 className='our-products-heading'>Our Products</h2>
       </header>
       <div className='products-with-aside'>
         <aside>
           <nav aria-label='products'>
             <ul>
-              <li><Link to='/products'>All Products</Link></li>
-              <li><Link to='/products/matcha'>Matcha</Link></li>
-              <li><Link to='/products/green-tea'>Green Teas</Link></li>
-              <li><Link to='/products/black-tea'>Black Teas</Link></li>
-              <li><Link to='/products/herbal-tea'>Herbal</Link></li>
+              <li>
+                <Link to='/products'>All Products</Link>
+              </li>
+              <li>
+                <Link to='/products/matcha'>Matcha</Link>
+              </li>
+              <li>
+                <Link to='/products/green-tea'>Green Teas</Link>
+              </li>
+              <li>
+                <Link to='/products/black-tea'>Black Teas</Link>
+              </li>
+              <li>
+                <Link to='/products/herbal-tea'>Herbal</Link>
+              </li>
             </ul>
           </nav>
         </aside>
         <Outlet />
       </div>
     </>
-  )
+  );
 }
 
 export default ProductsLayout;
